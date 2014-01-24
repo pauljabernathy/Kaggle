8bdb56c3f20004b69aafb2c1533c26f11e814d0e
paul <paul@quant>
2014-01-24 16:32:08 -0500

    some cleanup

--- src/titanic/TitanicNaiveBayes.java
+++ src/titanic/TitanicNaiveBayes.java
@@ -36,15 +36,21 @@ public class TitanicNaiveBayes implements TitanicStrategy {
     public static final int[] TEST_INDECES = /**new int[]{ 1, 3, 11 };///**/new int[]{ 2, 5};//, 7, 12, 13 };
     //public static final int SURVIVED_COLUMN = 1;
     
+    private ProbDist<Classification> probDist;
+    
     public static void main(String[] args) {
         TitanicNaiveBayes titanic = new TitanicNaiveBayes();
         titanic.runTrain("data/titanic.csv", "train_results.csv");
-        //titanic.runTest("titanic_test.csv", "test_results_20131110_1.csv", new int[]{ 5, 7, 8, 12, 13 });
+        //titanic.runTest("data/titanic_test.csv", "test_results_20140124.csv", new int[]{ 5, 7, 8, 12, 13 }, Constants.TRAINING_FILE, new int[]{ 5, 7, 8, 12, 13 });
         //titanic.runTest("titanic_test.csv", "test_results_20131110_2.csv", new int[]{ 5, 7, 8, 13 });
         //titanic.runTest("titanic_test.csv", "test_results_20131110_3.csv", new int[]{ 2, 5, 7 });
         //titanic.runTest("titanic_test.csv", "test_results_20131110_4.csv", new int[]{ 2, 5, 7, 8, 12 });
         //titanic.runTest("titanic_test.csv", "test_results_20131110_5.csv", new int[]{ 2, 5, 7, 8 });
         
+    }
+    
+    public TitanicNaiveBayes() {
+        this.probDist = new TitanicDistData().getClassificationDist();
     }
     
     public void runTrain(String inputFile, String outputFile) {
@@ -73,32 +79,18 @@ public class TitanicNaiveBayes implements TitanicStrategy {
     
     private Result doOneTrainingRun(String inputFile, String outputFile, int[] indeces) {
         //System.out.println("\ndoOneTrainingRun(" + inputFile + ", " + outputFile + ", " + Utilities.arrayToString(indeces));
-        //java.io.File f = new java.io.File("");
-        //System.out.println(f.getAbsolutePath());
         List<List> data = (List<List>)Titanic.loadData(inputFile, indeces, Titanic.COLUMN_SEPARATOR);
         List<String> correct = Titanic.loadCorrectClassifications(inputFile, Constants.SURVIVED_COLUMN, Titanic.COLUMN_SEPARATOR);
         
         Classifier classifier = new Classifier();
-        TitanicDistData titanicDists = new TitanicDistData();
-        classifier.setDist(titanicDists.getClassificationDist());
+        classifier.setDist(this.getProbDist());
         try {
             ProbDist<Classification> classDist = CSVReader.getDistributions(inputFile, Constants.SURVIVED_COLUMN, indeces, ",");
             classifier.setDist(classDist);
-            /**List<Classification> classifications = classDist.getValues();
-            for(Classification c : classifications) {
-                System.out.println(c.getName());
-                List<ProbDist> featureDists = c.getFeatureCPDs();
-                for(ProbDist dist : featureDists) {
-                    dist.display();
-                }
-                System.out.println();
-            }/**/
-            //classifier.setDist(classDist);
         } catch(Exception e) {
-            System.err.println(e.getClass() + " in train():  " + e.getMessage());
+            System.err.println(e.getClass() + " in doOneTrainingRun():  " + e.getMessage());
             return new Result(indeces, new ArrayList<String>(), 0.0);
         }
-        //doClassification(data, classifier);
         List<String> result = classifier.classify(data);
         int numCorrect = Titanic.compare(correct, result);
         double percent = (double)numCorrect / (double)data.size() * 100;
@@ -106,36 +98,26 @@ public class TitanicNaiveBayes implements TitanicStrategy {
         try {
             Titanic.writeToFile(result, outputFile, 1);
         } catch(IOException e) {
-            System.err.println(e.getClass() + " writing result file:  " + e.getMessage());
+            System.err.println(e.getClass() + " writing result file in doOneTrainingRun():  " + e.getMessage());
         }
         return new Result(indeces, result, percent);
     }
     
     public void runTest(String inputFile, String outputFile) {
-        runTest(inputFile, outputFile, TEST_INDECES);
+        runTest(inputFile, outputFile, TEST_INDECES, Constants.TRAINING_FILE, TEST_INDECES);
     }
     
-    public void runTest(String inputFile, String outputFile, int[] indeces) {
+    public void runTest(String inputFile, String outputFile, int[] indeces, String referenceFile, int[] referenceIndeces) {
         //List<List> data = (List<List>)loadData(inputFile, new int[]{ 1, 3, 11 }, Titanic.COLUMN_SEPARATOR);
         List<List> data = (List<List>)Titanic.loadData(inputFile, indeces, Titanic.COLUMN_SEPARATOR);
         Classifier classifier = new Classifier();
-        TitanicDistData titanicDists = new TitanicDistData();
-        classifier.setDist(titanicDists.getClassificationDist());
+        classifier.setDist(this.getProbDist());
         try {
-            //ProbDist<Classification> classDist = CSVReader.getDistributions(inputFile, SURVIVED_COLUMN, TEST_INDECES, ",");
-            ProbDist<Classification> classDist = CSVReader.getDistributions("titanic.csv", Constants.SURVIVED_COLUMN, indeces, ",");
-            List<Classification> classifications = classDist.getValues();
-            for(Classification c : classifications) {
-                System.out.println(c.getName());
-                List<ProbDist> featureDists = c.getFeatureCPDs();
-                for(ProbDist dist : featureDists) {
-                    dist.display();
-                }
-                System.out.println();
-            }
+            ProbDist<Classification> classDist = CSVReader.getDistributions(referenceFile, Constants.SURVIVED_COLUMN, referenceIndeces, ",");
+            List<Classification> cl = classDist.getValues();
             classifier.setDist(classDist);
         } catch(Exception e) {
-            System.err.println(e.getClass() + " in train:  " + e.getMessage());
+            System.err.println(e.getClass() + " in runTest():  " + e.getMessage());
             return;
         }
         List<String> result = classifier.classify(data);
@@ -144,7 +126,7 @@ public class TitanicNaiveBayes implements TitanicStrategy {
         try {
             Titanic.writeToFile(result, outputFile, 892);
         } catch(IOException e) {
-            System.err.println(e.getClass() + " writing result file:  " + e.getMessage());
+            System.err.println(e.getClass() + " writing result file in runTest():  " + e.getMessage());
         }
     }
     
@@ -160,6 +142,8 @@ public class TitanicNaiveBayes implements TitanicStrategy {
         return result;
     }
     
-    
+    public ProbDist<Classification> getProbDist() {
+        return this.probDist;
+    }
     
 }
